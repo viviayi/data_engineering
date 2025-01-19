a3c01bdd0a1a4c05c8633df9e8d4228546d3e69f
易玮玮 <741173912@qq.com>
2025-01-19 21:24:50 +0800

    terraform varibles

---
+++ homework/docker_sql/terraform/terraform_basic/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+#  credentials = 
+  project = "dtc-de-course-448111"
+  region  = "us-central1"
+}
+
+
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "bucket-20250119-448111"
+  location      = "US"
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "demodataset"
+  project    = "dtc-de-course-448111"
+  location   = "US"
+}
